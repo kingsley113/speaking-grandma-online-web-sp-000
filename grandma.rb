1ef952d896daedf78cce4ccbced07593d88be181
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-08 20:19:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,9 +14,10 @@
 
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    if phrase == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
+    else
+      "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
   end
